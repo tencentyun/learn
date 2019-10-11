400a00df1cb8d1add1e056248495a2323126a900
dengtao <taodengzhy@163.com>
2019-10-11 23:27:52 +0800

    react路由

--- 20 - react/imoocmanager/src/App.js
+++ 20 - react/imoocmanager/src/App.js
@@ -3,13 +3,9 @@ import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import './App.scss'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
-
-import Home from './demoTest/Home'
-import About from './demoTest/About'
-import Topic from './demoTest/Topic'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
 // function App() {
@@ -25,37 +21,27 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-          <ul>
-            <li>
-              <Link to='/'>home</Link>
-            </li>
-            <li>
-              <Link to='/about'>about</Link>
-            </li>
-            <li>
-              <Link to='/topic'>topic</Link>
-            </li>
-          </ul>
-          <br />
           <Switch>
-            <Route exact={true} path='/' component={Home}></Route>
-            <Route path='/about' component={About}></Route>
-            <Route path='/topic' component={Topic}></Route>
+            <Route exact={true} path='/'>
+              <Row>
+                <Col span={4}>
+                  <NavLeft></NavLeft>
+                </Col>
+                <Col span={20} className='main'>
+                  <Header></Header>
+                  <Row className='main-content'>
+                    <Home></Home>
+                  </Row>
+                  <Footer></Footer>
+                </Col>
+              </Row>
+            </Route>
+            {/* <Route path='/about/:id' component={About}></Route>
+            <Route path='/topic' component={Topic}></Route> */}
           </Switch>
         </div>
       </BrowserRouter>
-      // <Row>
-      //   <Col span={4}>
-      //   <NavLeft></NavLeft>
-      //   </Col>
-      //   <Col span={20} className='main'>
-      //     <Header></Header>
-      //     <Row className='main-content'>
-      //       <Home></Home>
-      //     </Row>
-      //     <Footer></Footer>
-      //   </Col>
-      // </Row>
+
     )
   }
 }
