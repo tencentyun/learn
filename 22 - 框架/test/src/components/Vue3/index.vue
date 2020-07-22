9dbd0c4d8ea3e013049658a6048ab884fd7aa416
邓涛 <taodengzhy@163.com>
2020-07-22 14:14:22 +0800

    vue3

---
+++ 22 - 框架/test/src/components/Vue3/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='Vue3'></div>
+</template>
+
+<script>
+export default {
+  name: 'Vue3',
+  data() {
+    return {
+      
+    }
+  },
+  components: {},
+  mounted() { },
+  methods: {
+    // Vue3升级内容
+    // 1. 全部用ts重写(响应式、vdom、模板编译等)
+    // 2. 性能提升，代码量减少
+    // 3. 会调整部分API(应该不会断崖式调整)
+
+    // Vue2.x马上就要过时了吗？
+    // 1. Vue3从正式发布到推广开来，还需要一段时间, vue是一个世界级的框架
+    // 2. Vue2应用范围非常广,有大量项目需要维护、升级
+    // 3. Proxy存在浏览器兼容性问题，且不能polyfill
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
